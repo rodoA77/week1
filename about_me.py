e6f38f18eed07ce8f56b1a0588264a4d009b9d46
RodoPAG <ra.pagani.sanchez@gmail.com>
2026-03-22 17:31:12 -0300

    add about_me.py with variables

---
+++ about_me.py
@@ -0,0 +1,9 @@
+name = "Rodo"
+age = 48
+city = "Buenos Aires"
+hobby = "coding"
+
+print ("My name is ", name)
+print ("I am ", age, " years old")
+print ("I live in ", city)
+print ("My new hobby is ", hobby)
