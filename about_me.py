c4c0d4944f513623bab35e1c12ec949cde01c37a
RodoPAG <ra.pagani.sanchez@gmail.com>
2026-03-22 17:53:45 -0300

    personalize about_me. with real story

--- about_me.py
+++ about_me.py
@@ -1,9 +1,17 @@
 name = "Rodo"
 age = 48
 city = "Buenos Aires"
-hobby = "coding"
+born = "Tucuman"
+lived = "Ledesma, Jujuy"
+hobby = "learning"
+new_hobby = "coding"
+years_coding = 2026 - 2026
 
 print ("My name is ", name)
 print ("I am ", age, " years old")
 print ("I live in ", city)
-print ("My new hobby is ", hobby)
+print ("But, I'm not porteño... I was born in ", born)
+print ("I lived in ", lived)
+print ("My all time favorite hobby is ", hobby)
+print ("But, I also like ", new_hobby)
+print ("Years coding: ", years_coding)
